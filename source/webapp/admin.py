e3956f80f6252be910f0f18e21708a69a1942f62
mtashtanovski <mtashtanovski@gmail.com>
2022-01-13 18:32:33 +0600

    1. Created models of Issue, Status, Type. 2. Registered all models in admin.py. 3. Made necessary migrations. 3. Added some data of issues with statuses and types. 4. Dumped all data into dump_issue.json

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import IssueModel, StatusModel, TypeModel
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'description', 'status', 'type', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['status', 'type']
+    fields = ['summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(IssueModel, IssueAdmin)
+admin.site.register(StatusModel)
+admin.site.register(TypeModel)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
+
 # Create your models here.
+class IssueModel(models.Model):
+    summary = models.CharField(max_length=200, null=False, blank=False, verbose_name="Заголовок")
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Описание")
+    status = models.ForeignKey('webapp.StatusModel', on_delete=models.PROTECT,
+                               related_name='status', verbose_name="Статус")
+    type = models.ForeignKey('webapp.TypeModel', on_delete=models.PROTECT,
+                             related_name='type', verbose_name="Тип")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f"{self.pk}. {self.summary}, {self.type}, {self.status}."
+
+    class Meta:
+        db_table = 'issues'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+
+class StatusModel(models.Model):
+    title = models.CharField(max_length=32, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        db_table = 'statuses'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class TypeModel(models.Model):
+    title = models.CharField(max_length=32, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        db_table = 'types'
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
