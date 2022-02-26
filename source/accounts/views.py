e05eb77a92c12ffaa71da7d78cf0169b82b1437b
mtashtanovski <mtashtanovski@gmail.com>
2022-02-26 22:16:56 +0600

    Homework #60. 1. Added pagination for projects list and modified partials. 2. Modified view of profile page.

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 
 # Create your views here.
 from django.urls import reverse
@@ -50,7 +52,27 @@ def logout_view(request):
     return redirect('webapp:index')
 
 
-class UserProfileView(DetailView):
+class UserProfileView(LoginRequiredMixin, DetailView):
     model = get_user_model()
     template_name = 'profile.html'
     context_object_name = 'user_object'
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        paginator = Paginator(
+            self.get_object().projects.all(),
+            self.paginate_related_by,
+            self.paginate_related_orphans,
+        )
+
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+
+        kwargs['page_obj'] = page
+        kwargs['projects'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+
+        return super(UserProfileView, self).get_context_data(**kwargs)
+
+
